a16a098e51435eb5c9569f74405462ff41481be0
DavoDev-Hub <juanpajmz13@gmail.com>
2025-07-10 11:33:23 -0600

    schemas correction
    activiad and conferencias correction

--- BackEnd/src/schemas/conferencia.schema.ts
+++ BackEnd/src/schemas/conferencia.schema.ts
@@ -1,9 +1,11 @@
-import { z } from 'zod'
+import { z } from "zod";
 
 export const conferenciaSchema = z.object({
-    nombre: z.string().min(3),
-    descripcion: z.string().min(5),
-    imagen: z.string(),
-    horaInicio: z.string(),
-    horaFin: z.string(),
-}); 
+    nombre: z.string().min(3, "El nombre debe tener al menos 3 caracteres"),
+    ponente: z.string().min(3, "El ponente debe tener al menos 3 caracteres"),
+    descripcion: z.string().min(5, "La descripción debe tener al menos 5 caracteres"),
+    lugar: z.string().min(3, "El lugar debe tener al menos 3 caracteres"),
+    fecha: z.coerce.date({ invalid_type_error: "La fecha debe ser una fecha válida" }),
+    hora: z.coerce.date({ invalid_type_error: "La hora debe ser una fecha/hora válida" }),
+});
+
