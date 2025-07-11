520ea36b248bcef73bb77927e7600162b1be14b2
DavoDev-Hub <juanpajmz13@gmail.com>
2025-07-11 12:40:58 -0600

    add modal

--- FrontEnd/src/src/lib/utils.ts
+++ FrontEnd/src/src/lib/utils.ts
@@ -1,5 +1,7 @@
-import { ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-variants"
+import { clsx } from "clsx"
+import type { ClassValue } from "clsx"
+import { twMerge } from "tailwind-merge"
+
 
 export function cn(...inputs: ClassValue[]) {
     return twMerge(clsx(inputs))
