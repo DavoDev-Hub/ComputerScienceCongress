64e9042fde7023e87f5c0249403a6bcde2ccb94b
DavoDev-Hub <juanpajmz13@gmail.com>
2025-07-16 17:25:48 -0600

    finished asistencia endpoint

--- BackEnd/src/routes/asistencia.routes.ts
+++ BackEnd/src/routes/asistencia.routes.ts
@@ -2,11 +2,15 @@ import { Router } from "express"
 import {
     registrarAsistencia,
     getAllAsistencias,
+    getAsistenciasPorAlumno,
+    deleteAsistencia
 } from "../controllers/asistencia.controller"
 const router = Router()
 
 router.post("/", registrarAsistencia)
 router.get("/", getAllAsistencias)
+router.get("/alumno/:id", getAsistenciasPorAlumno)
+router.delete("/:id", deleteAsistencia)
 
 export default router
 
